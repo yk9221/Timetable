54cee5d534eaf35bbf40900df332fc5a89816519
yk9221 <ykyun52@gmail.com>
2023-07-08 03:16:48 +0900

    created server.js

---
+++ server.js
@@ -0,0 +1,213 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/:dynamic", (req, res) => {
+    const {key} = req.query;
+
+    const course_promise = get_matches(key);
+    
+    course_promise.then(result => {
+        let course_code = result[0].course_code;
+        let course_name = result[0].course_name;
+        let course_section = result[0].course_section;
+
+        const course_info = get_course_info(course_code, course_name, course_section);
+
+        course_info.then(data => {
+            res.status(200).json(data);
+        });
+    });
+
+});
+
+async function get_matches(course) {
+    let course_list;
+
+    await fetch("https://api.easi.utoronto.ca/ttb/getOptimizedMatchingCourseTitles?term=" + course + "&divisions=APSC&sessions=20239&lowerThreshold=50&upperThreshold=200")
+    .then(response => response.text())
+    .then(data => {
+        course_list = parse_matches(data);
+    });
+
+    return course_list;
+}
+
+async function get_course_info(course_code, course_name, course_section) {
+    let course_info;
+
+    await fetch("https://api.easi.utoronto.ca/ttb/getPageableCourses", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "courseCodeAndTitleProps":{
+                "courseCode": course_code,
+                "courseTitle": course_name,
+                "courseSectionCode": course_section,
+                "searchCourseDescription": false
+            },
+            "departmentProps": [],
+            "campuses": [],
+            "sessions": ["20239"],
+            "requirementProps": [],
+            "instructor": "",
+            "courseLevels": [],
+            "deliveryModes": [],
+            "dayPreferences": [],
+            "timePreferences": [],
+            "divisions": ["APSC"],
+            "creditWeights": [],
+            "page": 1,
+            "pageSize": 20,
+            "direction": "asc"
+        })
+    })
+    .then(response => {
+        if(response.ok) {
+            return response.text();
+        }
+        else {
+            console.log("Error");
+        }
+    })
+    .then(data => {
+        course_info = parse_course_info(course_code, data);
+    })
+    .catch(error => {
+        console.log("Error: ", error);
+    });
+
+    return course_info;
+}
+
+function parse_matches(data) {
+    const cheerio = require("cheerio");
+
+    const full_html = cheerio.load(data);
+    const code_elements = full_html("code");
+    const name_elements = full_html("name");
+    const section_elements = full_html("sectionCode");
+
+    const course_list = new Array();
+
+    const max_length = Math.max(code_elements.length, name_elements.length, section_elements.length);
+
+    for(let i = 0; i < max_length; ++i) {
+        if(i < code_elements.length) {
+            const code_element = full_html(code_elements[i]);
+            const code_parent = code_element.parent();
+
+            if(code_parent.get(0).tagName == "codesandtitles") {
+                course_list[i / 2] = {
+                    course_code: code_element.text()
+                };
+            }
+        }
+
+        if(i < name_elements.length) {
+            const name_element = full_html(name_elements[i]);
+            const name_parent = name_element.parent();
+            if(name_parent.get(0).tagName == "codesandtitles") {
+                course_list[i / 2]["course_name"] = name_element.text();
+            }
+        }
+
+        if(i < section_elements.length) {
+            const section_element = full_html(section_elements[i]);
+            const section_parent = section_element.parent();
+            if(section_parent.get(0).tagName == "codesandtitles") {
+                course_list[i / 2]["course_section"] = section_element.text();
+            }
+        }
+    }
+
+    return course_list;
+}
+
+function parse_course_info(course_code, data) {
+    const cheerio = require("cheerio");
+
+    const weekday_map = new Map([
+        [1, "Monday"],
+        [2, "Tuesday"],
+        [3, "Wednesday"],
+        [4, "Thursday"],
+        [5, "Friday"]
+    ]);
+
+    const course_info = new Array();
+
+    const full_html = cheerio.load(data);
+    const section_elements = full_html("sections");
+
+    for(let i = 0; i < section_elements.length; ++i) {
+        const section_element = full_html(section_elements[i]);
+        const section_parent = section_element.parent();
+
+        if(section_parent.get(0).tagName == "sections") {
+            const part_html = cheerio.load(section_element.html());
+            const teach_method = part_html("teachmethod");
+            const section_number = part_html("sectionnumber");
+            const starts = part_html("start");
+            const ends = part_html("end");
+
+            course_info[i - 1] = {
+                course_code: course_code,
+                teach_method: teach_method.text(),
+                section_number: section_number.text()
+            };
+            course_info[i - 1]["time"] = new Array();
+
+
+            for(let j = 0; j < starts.length; ++j) {
+                const start = part_html(starts[j]);
+                const end = part_html(ends[j]);
+                const day = start.text()[0];
+
+                course_info[i - 1]["time"].push({
+                    start_time: convert_miliseconds(start.text().substring(1)),
+                    end_time: convert_miliseconds(end.text().substring(1)),
+                    day: weekday_map.get(parseInt(day))
+                });
+            }
+        }
+    }
+
+    return course_info;
+}
+
+function convert_miliseconds(time) {
+    const miliseconds_per_day = 86400000;
+    const hours_per_day = 24;
+
+    return time / miliseconds_per_day * hours_per_day;
+}
+
+// app.post("/", (req, res) => {
+//     const {course} = req.body;
+
+//     const course_promise = get_matches(course);
+    
+//     course_promise.then(result => {
+//         let course_code = result[0].course_code;
+//         let course_name = result[0].course_name;
+//         let course_section = result[0].course_section;
+        
+//         get_course_info(course_code, course_name, course_section);
+//     });
+
+//     res.status(200).send({status: "received"})
+
+
+//     if(!course) {
+//         return res.status(400).send({status: "failed"})
+//     }
+//     res.status(200).send({status: "received"})
+// });
+
+app.listen(port, () => console.log("server started"));
