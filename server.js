f2675728827e06c8717a0bd7d0071dbe053d7fcd
yk9221 <ykyun52@gmail.com>
2023-12-19 20:18:08 -0500

    revert push

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("./"));
+app.use(express.static("public"));
 app.use(express.json());
 
 // api fetch with get
